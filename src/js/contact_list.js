c3316dccc57256e87f23906ab02f2ea9f0964631
Ben Lee <leeohsunshine@gmail.com>
2016-04-01 11:04:43 -0400

    Added Contact list page and functionality

--- src/js/contact_list.js
+++ src/js/contact_list.js
@@ -1,1 +1,28 @@
 //Contact List Page//
+import React, {Component, PropTypes} from 'react';
+
+export default class ContactList extends Component {
+  static propTypes = {
+    info: PropTypes.array.isRequired,
+    onContactSelect: PropTypes.func.isRequired
+  };
+  makeListItem(user){
+    let {onContactSelect} = this.props
+
+    return (
+      <li onClick={onContactSelect.bind(null, user)}>
+        <img src={user.imageURL}/>
+        {user.name}
+      </li>
+    )}
+  render() {
+    let {info} = this.props
+    return (
+      <div className='contact-list'>
+        <ul>
+          {info.map(::this.makeListItem)}
+        </ul>
+      </div>
+    );
+  }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ContactDetails from './contact_details';
+import ContactList from './contact_list';
+import CONTACTS_DATA from './contacts_data';
 
 let user = {
   imageURL: 'http://fillMurray.com/1100/1100',
@@ -14,7 +16,23 @@ let user = {
 function tempFunc(){
   alert('dude');
 }
-ReactDOM.render(
-  <ContactDetails user={user} goBack={tempFunc} />,
+
+function renderContactDetails(user){
+  ReactDOM.render(
+  <ContactDetails
+  user={user}
+  goBack={renderContactList}
+  />,
   document.querySelector('.app')
-)
+)}
+
+function renderContactList(){
+  ReactDOM.render(
+  <ContactList
+  info={CONTACTS_DATA}
+  onContactSelect={renderContactDetails}
+  />,
+  document.querySelector('.app')
+)}
+
+renderContactList()
